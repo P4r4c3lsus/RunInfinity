b0af47aef87051490a1242755c492f2f5272752e
Yormama <caceresdiego560@gmail.com>
2026-09-16 20:52:13 -0600

    Initial commit

---
+++ Assets/Scripts/UIWindow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public virtual void Show()
+    {
+
+    }
+
+    public virtual void Hide()
+    {
+
+    }
+}
